5664a079a69f70a5208479c506467b5fd9431086
Vincent Masiello <vincentmasiello@gmail.com>
2017-08-31 00:11:54 -0400

    bedtime commit. Next up: addMessage (in App.jsx) should add activeUser as the author

--- components/messages/Message.jsx
+++ components/messages/Message.jsx
@@ -1,3 +1,23 @@
 import React, {Component} from 'react';
 
+class Message extends Component {
+	render() {
+		// don't think we need the messages to be links unless we need to
+		// link the author's name to something (maybe support DMs later)
+		const {message} = this.props;
+		// TODO: check if messsage.author === activeUser.name, highlight users' 
+		// own messages.
+		return (
+			<li>
+				<strong>{message.author}&nbsp;&nbsp;&nbsp;</strong>
+				<span>{message.name}</span>
+			</li>
+		)
+	}
+}
+
+Message.propTypes = {
+	message: React.PropTypes.object.isRequired
+}
+
 export default Message
