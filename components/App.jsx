228bb0f4abd9ea4a213a6a25542a0df12c61d9be
Vincent Masiello <vincentmasiello@gmail.com>
2017-08-30 23:16:28 -0400

    render ChannelSection from App.jsx

--- components/App.jsx
+++ components/App.jsx
@@ -5,12 +5,14 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			channels: []
+			channels: [],
+			users: []
 		};
 	}
 	addChannel(name) {
 		let {channels} = this.state;
 
+		// TODO: validate unique channelName
 		// shorthand sets value of name to 'name' prop
 		channels.push({id: channels.length, name});
 
@@ -20,7 +22,38 @@ class App extends Component {
 	}
 	setChannel(activeChannel) {
 		this.setState({activeChannel});
-		// TODO: Get Channels Messages from server
+		// TODO: send message to server "{user} Joined channel {channel} @ Date.now()"
+		// TODO: Get Channel's Messages from server (clear old messages)
+	}
+
+	addUser(name) {
+		let {users} = this.state;
+		//TODO: validate unique username
+		users.push({id: users.length, name});
+		this.setState({users});
+		// TODO: send to server
+	}
+	setUser(activeUser) {
+		this.setState({activeUser});
+	}
+
+	/* 
+	 * removeUser takes a name(string) and removes the corresponding 
+	 * user from the list, updating state
+	 * 
+	 * TODO: would love to write a better data structure but in the 
+	 *			context of this simple app that may be overkill.
+	 */
+	removeUser(name) {
+		let {users} = this.props;
+		users.find((usr, index) => {
+			if (usr.name === name) {
+				users.slice(index);
+				return true;
+			}
+			return false;
+		});
+		this.setState({users});
 	}
 	render() {
 		return (
@@ -30,6 +63,12 @@ class App extends Component {
 						{...this.state}
 						addChannel={this.addChannel.bind(this)}
 						setChannel={this.setChannel.bind(this)}
+					/>
+					<br />
+					<UserSection
+						{...this.state}
+						addUser={this.addUser.bind(this)}
+						setUser={this.setUser.bind(this)}
 					/>
 				</div>
 			</div>
